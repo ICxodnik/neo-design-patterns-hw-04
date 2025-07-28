b58016fd1a2eee8c782b037692c2dccfcf73b987
Natalia <icxodnik@gmail.com>
2025-07-28 22:48:01 +0200

    add implementation

--- src/prototype/UserProfile.ts
+++ src/prototype/UserProfile.ts
@@ -1,1 +1,23 @@
-// TODO: реалізуйте клас або інтерфейс тут
+import { UserProfilePrototype } from './UserProfilePrototype';
+
+type Permissions = {
+  canEditUsers: boolean;
+  canApproveBudget: boolean;
+  canAccessInternalTools: boolean;
+};
+
+type Department = 'finance' | 'engineering' | 'marketing';
+
+export class UserProfile implements UserProfilePrototype {
+  constructor(
+    public username: string,
+    public department: Department,
+    public permissions: Permissions
+  ) {}
+
+  clone() {
+    return new UserProfile(this.username, this.department, {
+      ...this.permissions,
+    });
+  }
+}
--- src/singleton/AppConfigService.ts
+++ src/singleton/AppConfigService.ts
@@ -1,6 +1,24 @@
 export class AppConfigService {
-    constructor(
-      public readonly companyName: string,
-      public readonly footer: string
-    ) {}
+  private static instance: AppConfigService;
+  private config: Record<string, any> = {};
+
+  private constructor(
+    public readonly companyName: string,
+    public readonly footer: string
+  ) {}
+
+  public static getInstance(companyName: string, footer: string) {
+    if (!AppConfigService.instance) {
+      AppConfigService.instance = new AppConfigService(companyName, footer);
+    }
+    return AppConfigService.instance;
   }
+
+  set(key: string, value: any): void {
+    this.config[key] = value;
+  }
+
+  get(key: string): any {
+    return this.config[key];
+  }
+}
